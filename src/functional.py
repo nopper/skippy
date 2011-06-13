099d715750fb1169cdf10229238c818b43c7a8f3
nopper <stack.box@gmail.com>
2011-06-13 17:44:40 +0200

    Add new linsolve function to the functional code

--- src/functional.py
+++ src/functional.py
@@ -1,4 +1,5 @@
-import math
+import numpy
+import numpy.linalg
 
 offsets = ((-1,0), (0,-1), (0,1), (1,0))
 
@@ -19,6 +20,36 @@ def variance(a, b):
         return [n, mean, m2]
 
 function = variance
+
+def linsolve(a, b):
+    """
+    Here we try to interpret the matrix as follows
+      2
+    1 5 3
+      4
+
+    5 will be our a parameter. The aim of the function is to find a solution to
+    the given algebrical system:
+     1x + 3y = 5
+     2x + 4y = 5
+
+    Then the mean value is calculated and putted in the center.
+    """
+    if isinstance(a, int):
+        return [a, b]
+    elif isinstance(a, list):
+        a.append(b)
+
+        if len(a) == 5:
+            equations = numpy.array(((a[1], a[4]),
+                                     (a[2], a[3])))
+            given     = numpy.array((a[0], a[0]))
+            try:
+                return numpy.linalg.solve(equations, given).mean()
+            except:
+                return 0
+        return a
+
 
 #offsets = ((-2, -1), (-2, 0), (-2, -2))
 function = min
